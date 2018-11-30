8f9b32085368cec392b8e6292d2c6e465f15e4e1
Alba Cárdenas <albillana@MacBook-Pro-3.local>
2018-11-29 23:28:49 -0300

    Add article model method to add info to xslx

--- app/models/blog/article.rb
+++ app/models/blog/article.rb
@@ -1,5 +1,12 @@
 module Blog
   class Article < ApplicationRecord
     validates_presence_of :title, :content, :author_id
+
+    def write_workbook(book)
+      book.add_worksheet(name: 'Article') do |sheet|
+        sheet.add_row %w[ID Title Content Author]
+        sheet.add_row [id, title, content, author_id]
+      end
+    end
   end
 end
