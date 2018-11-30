2e502b151b93e2900c0375f6960ea84fadb4cb70
Alba Cárdenas <albillana@MacBook-Pro-3.local>
2018-11-30 00:31:20 -0300

    add replies relations to comment

--- app/models/blog/comment.rb
+++ app/models/blog/comment.rb
@@ -1,6 +1,8 @@
 module Blog
   class Comment < ApplicationRecord
     belongs_to :article, foreign_key: 'blog_article_id'
+    has_many :replies, class_name: 'Comment', foreign_key: 'parent_id',
+      dependent: :destroy
     validates_presence_of :text
   end
 end
