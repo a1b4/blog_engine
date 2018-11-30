353ec4e1704a406c83f1d47471fe45724b89b49e
Alba Cárdenas <albillana@MacBook-Pro-3.local>
2018-11-29 23:57:45 -0300

    Add create action to comments controller

--- app/controllers/blog/comments_controller.rb
+++ app/controllers/blog/comments_controller.rb
@@ -8,5 +8,23 @@ module Blog
 
       render json: @article.comments.to_json
     end
+
+    def create
+      @article = Article.find_by_id(params[:article_id])
+      return not_found_response('Article') unless @article
+
+      @comment = @article.comments.create(comment_params)
+      if @comment.valid?
+        render json: @article.to_json
+      else
+        errors_response(@comment)
+      end
+    end
+
+    private
+
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
   end
 end
