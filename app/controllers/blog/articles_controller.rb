722769c00f4a0dc7a26aa27031d2e94822f3a5d2
Alba Cárdenas <albillana@MacBook-Pro-3.local>
2018-11-29 22:28:33 -0300

    Add article update action

--- app/controllers/blog/articles_controller.rb
+++ app/controllers/blog/articles_controller.rb
@@ -8,15 +8,30 @@ module Blog
 
     def create
       @article = Article.new(article_params)
-      return render json: { errors: @article.errors }, status: :bad_request unless @article.save
+      return errors_response unless @article.save
 
       render json: @article.to_json
+    end
+
+    def update
+      @article = Article.find_by_id(params[:id])
+      return render json: { error: 'Article does not exist' }, status: :not_found unless @article
+
+      if @article.update(article_params)
+        render json: @article.to_json
+      else
+        errors_response
+      end
     end
 
     private
 
     def article_params
       params.require(:article).permit(:title, :content, :author_id)
+    end
+
+    def errors_response
+      render json: { errors: @article.errors }, status: :bad_request
     end
   end
 end
