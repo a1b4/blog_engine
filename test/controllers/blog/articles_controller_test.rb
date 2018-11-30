fdd68c5bb05749421c4eb2a608ae9de174d38484
Alba Cárdenas <albillana@MacBook-Pro-3.local>
2018-11-29 22:38:42 -0300

    Add article update tests

--- test/controllers/blog/articles_controller_test.rb
+++ test/controllers/blog/articles_controller_test.rb
@@ -6,6 +6,7 @@ module Blog
 
     setup do
       @routes = Engine.routes
+      @article = blog_articles(:valid)
     end
 
     test 'should get index' do
@@ -23,6 +24,24 @@ module Blog
       article = { title: 'title' }
       post articles_url, params: { article: article }
       assert_response :bad_request
+    end
+
+    test 'should post update' do
+      article_updates = { title: 'new article title' }
+      patch article_url(@article.id), params: { article: article_updates }
+      assert_response :success
+    end
+
+    test 'post update should return errors' do
+      article_updates = { title: nil }
+      patch article_url(@article.id), params: { article: article_updates }
+      assert_response :bad_request
+    end
+
+    test 'post update should return not found' do
+      article_updates = { title: 'new article title' }
+      patch article_url(10), params: { article: article_updates }
+      assert_response :not_found
     end
   end
 end
