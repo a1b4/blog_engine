0c33f36401438821df34a592a0425cecd4106072
Alba Cárdenas <albillana@MacBook-Pro-3.local>
2018-11-29 22:16:30 -0300

    Add article create action

--- app/controllers/blog/articles_controller.rb
+++ app/controllers/blog/articles_controller.rb
@@ -5,5 +5,18 @@ module Blog
     def index
       render json: Article.all.to_json
     end
+
+    def create
+      @article = Article.new(article_params)
+      return render json: { errors: @article.errors }, status: :bad_request unless @article.save
+
+      render json: @article.to_json
+    end
+
+    private
+
+    def article_params
+      params.require(:article).permit(:title, :content, :author_id)
+    end
   end
 end
