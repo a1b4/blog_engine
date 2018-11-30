9decfd90142edeacc631ead8045be178159cf8b6
Alba Cárdenas <albillana@MacBook-Pro-3.local>
2018-11-29 22:21:04 -0300

    Add article create tests

--- test/controllers/blog/articles_controller_test.rb
+++ test/controllers/blog/articles_controller_test.rb
@@ -12,5 +12,17 @@ module Blog
       get articles_url
       assert_response :success
     end
+
+    test 'should post create' do
+      article = { title: 'title', content: 'content', author_id: 1 }
+      post articles_url, params: { article: article }
+      assert_response :success
+    end
+
+    test 'should post create return errors' do
+      article = { title: 'title' }
+      post articles_url, params: { article: article }
+      assert_response :bad_request
+    end
   end
 end
