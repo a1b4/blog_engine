5114921816ef1c02feafc4696244e4468bdb26e9
Alba Cárdenas <albillana@MacBook-Pro-3.local>
2018-11-29 23:50:09 -0300

    Add comments index action tests

---
+++ test/controllers/blog/comments_controller_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+module Blog
+  class CommentsControllerTest < ActionDispatch::IntegrationTest
+    include Engine.routes.url_helpers
+
+    setup do
+      @routes = Engine.routes
+      @article = blog_articles(:valid)
+    end
+
+    test 'should get index' do
+      get article_comments_url(@article.id)
+      assert_response :success
+    end
+
+    test 'get index should return not found' do
+      get article_comments_url(10)
+      assert_response :not_found
+    end
+  end
+end
