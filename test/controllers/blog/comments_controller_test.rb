b0024f70e841d2a4fd58ccdd183ab96695bcda9b
Alba Cárdenas <albillana@MacBook-Pro-3.local>
2018-11-30 00:00:48 -0300

    Add comments create tests

--- test/controllers/blog/comments_controller_test.rb
+++ test/controllers/blog/comments_controller_test.rb
@@ -18,5 +18,23 @@ module Blog
       get article_comments_url(10)
       assert_response :not_found
     end
+
+    test 'should post create' do
+      comment = { text: 'my new comment' }
+      post article_comments_url(@article.id), params: { comment: comment }
+      assert_response :success
+    end
+
+    test 'post create should return errors' do
+      comment = { text: nil }
+      post article_comments_url(@article.id), params: { comment: comment }
+      assert_response :bad_request
+    end
+
+    test 'post create should return not found' do
+      comment = { text: 'my new comment' }
+      post article_comments_url(10), params: { comment: comment }
+      assert_response :not_found
+    end
   end
 end
